c6834bb2e3fc523bc96203ffd20c62bf10da2d8f
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2022-11-14 11:19:54 +0530

    Update apipyth.py

--- apipyth.py
+++ apipyth.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import mysql.connector
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
